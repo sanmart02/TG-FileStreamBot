abccb62b5bc36f7b425b6162d98ba83d54c1c244
sanmart02 <113845440+sanmart02@users.noreply.github.com>
2023-11-27 15:27:10 -0300

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -32,7 +32,7 @@ async def media_receive_handler(_, m: Message):
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
-    logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    logger.info(f"Link direto: {stream_link} for {m.from_user.first_name} /n /n Link para navegador: {short_link}")
     try:
         await m.reply_text(
             text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
@@ -41,7 +41,7 @@ async def media_receive_handler(_, m: Message):
             quote=True,
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("💻 Assistir", url=short_link)]]
+                [[InlineKeyboardButton("💻 NAVEGADOR", url=short_link) InlineKeyboardButton("PLAYER", url=stream_link)]]
             ),
         )
     except errors.ButtonUrlInvalid:
